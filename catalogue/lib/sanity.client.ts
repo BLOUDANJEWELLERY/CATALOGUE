b210461552373d98946d3152d1cc489253c65b32
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-05 11:08:49 +0000

    modified:   catalogue/lib/sanity.client.ts 	modified:   catalogue/pages/catalogue/[page].tsx

--- catalogue/lib/sanity.client.ts
+++ catalogue/lib/sanity.client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "YOUR_SANITY_PROJECT_ID", // from Sanity Studio
+  projectId: "lfss7ezq", // from Sanity Studio
   dataset: "production",
   apiVersion: "2023-01-01",
   useCdn: true,
