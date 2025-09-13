b40b6d49456f125ace95949bbe614e36672581e0
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-13 13:44:28 +0300

    Update sanity.client.ts

--- catalogue/lib/sanity.client.ts
+++ catalogue/lib/sanity.client.ts
@@ -1,4 +1,6 @@
+// lib/sanity.client.ts
 import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   projectId: "lfss7ezq",
@@ -7,3 +9,11 @@ export const client = createClient({
   useCdn: false, // false because we will write data
   token: process.env.SANITY_WRITE_TOKEN, // token with write permission
 });
+
+// Create a builder for images
+const builder = imageUrlBuilder(client);
+
+// Export urlFor so your PDF generator can use it
+export function urlFor(source: any) {
+  return builder.image(source);
+}
