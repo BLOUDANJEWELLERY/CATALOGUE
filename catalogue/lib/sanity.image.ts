155fdc7658c65361da7fe757d1391617cc3570a6
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-13 13:50:31 +0300

    Update sanity.image.ts

--- catalogue/lib/sanity.image.ts
+++ catalogue/lib/sanity.image.ts
@@ -1,12 +1,21 @@
 import imageUrlBuilder from "@sanity/image-url";
 import { client } from "./sanity.client";
 
+// Minimal type for Sanity image
+export interface SanityImage {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
 const builder = imageUrlBuilder(client);
 
 /**
  * Generates a URL for a Sanity image.
  * @param source - Sanity image object
  */
-export function urlFor(source: object) {
+export function urlFor(source: SanityImage) {
   return builder.image(source);
 }
