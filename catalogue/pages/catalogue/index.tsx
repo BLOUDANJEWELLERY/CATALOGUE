dfc0b1518c7627e5ca642aa31ee21b3c225a7323
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-13 20:14:48 +0300

    Update index.tsx

--- catalogue/pages/catalogue/index.tsx
+++ catalogue/pages/catalogue/index.tsx
@@ -320,8 +320,8 @@ const handleDownloadPDF = async (filter: "Adult" | "Kids" | "Both") => {
       tempText.innerText = `B${item.modelNumber}`;
       tempText.style.fontWeight = "700";
       tempText.style.color = "#0b1a3d";
-      tempText.style.marginTop = "0px"; 
-      tempText.style.marginBottom = "4px";
+      tempText.style.marginTop = "-1px"; 
+      tempText.style.marginBottom = "2px";
       tempText.style.fontSize = "35px";
       tempText.style.lineHeight = "0.8"; // remove extra spacing
       tempDiv.appendChild(tempText);
@@ -332,7 +332,7 @@ const handleDownloadPDF = async (filter: "Adult" | "Kids" | "Both") => {
         p.innerText = `${label}${weight ? ` - ${weight}g` : ""}`;
         p.style.fontSize = "12px";
         p.style.color = "#0b1a3d";
-        p.style.margin = "0"; 
+        p.style.marginTop = "2px"; 
         tempDiv.appendChild(p);
       };
 
