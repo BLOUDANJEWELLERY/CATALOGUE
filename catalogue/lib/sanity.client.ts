e66d3c622dfd6374b485101b8470f1a8b805c97b
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-13 13:50:14 +0300

    Update sanity.client.ts

--- catalogue/lib/sanity.client.ts
+++ catalogue/lib/sanity.client.ts
@@ -1,19 +1,9 @@
-// lib/sanity.client.ts
 import { createClient } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageObject } from "next-sanity"; // correct type
 
 export const client = createClient({
   projectId: "lfss7ezq",
   dataset: "production",
   apiVersion: "2023-01-01",
-  useCdn: false, // false because we will write data
-  token: process.env.SANITY_WRITE_TOKEN, // token with write permission
+  useCdn: false,
+  token: process.env.SANITY_WRITE_TOKEN,
 });
-
-const builder = imageUrlBuilder(client);
-
-// Properly typed urlFor function
-export function urlFor(source: SanityImageObject) {
-  return builder.image(source);
-}
