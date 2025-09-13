113a2be001160187c35357802e5a89804e11de9b
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-13 20:38:26 +0300

    Update index.tsx

--- catalogue/pages/catalogue/index.tsx
+++ catalogue/pages/catalogue/index.tsx
@@ -321,7 +321,7 @@ const handleDownloadPDF = async (filter: "Adult" | "Kids" | "Both") => {
       tempText.style.fontWeight = "700";
       tempText.style.color = "#0b1a3d";
       tempText.style.marginTop = "-2px";
-      tempText.style.marginBottom = "10px"; // slightly closer to weights
+      tempText.style.marginBottom = "12px"; // slightly closer to weights
       tempText.style.fontSize = "35px";
       tempText.style.lineHeight = "0.8";
       tempDiv.appendChild(tempText);
@@ -332,7 +332,7 @@ const handleDownloadPDF = async (filter: "Adult" | "Kids" | "Both") => {
       weightContainer.style.width = "100%";
       weightContainer.style.justifyContent = "center"; // default center
       weightContainer.style.marginTop = "0px";
-      weightContainer.style.gap = "5px";
+      weightContainer.style.gap = "13px";
       tempDiv.appendChild(weightContainer);
 
       const adultWeightExists = item.sizes?.includes("Adult") && item.weightAdult;
