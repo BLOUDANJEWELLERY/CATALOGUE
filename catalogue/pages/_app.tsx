25b4706d6c9da7ede2eba290c6cac3040c82fd84
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-13 21:00:58 +0300

    Update _app.tsx

--- catalogue/pages/_app.tsx
+++ catalogue/pages/_app.tsx
@@ -1,6 +1,26 @@
+// pages/_app.tsx
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.PNG" />
+
+        {/* Optional: App logo for mobile devices */}
+        <link rel="apple-touch-icon" href="/favicon.PNG" />
+
+        {/* Optional: Page title */}
+        <title>BLOUDAN JEWELLERY - BANGLES CATALOGUE</title>
+
+        {/* Optional: Meta description */}
+        <meta name="description" content="Luxury bangles and jewellery collection" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
