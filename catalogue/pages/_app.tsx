81e639a2900d319b48827010a95cd280fa2158b2
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-14 17:31:34 +0300

    Update _app.tsx

--- catalogue/pages/_app.tsx
+++ catalogue/pages/_app.tsx
@@ -2,6 +2,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import RouteLoader from "../components/RouteLoader";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,16 +11,23 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* Favicon */}
         <link rel="icon" href="/favicon.PNG" />
 
-        {/* Optional: App logo for mobile devices */}
+        {/* App logo for mobile devices */}
         <link rel="apple-touch-icon" href="/favicon.PNG" />
 
-        {/* Optional: Page title */}
+        {/* Page title */}
         <title>BLOUDAN JEWELLERY - BANGLES CATALOGUE</title>
 
-        {/* Optional: Meta description */}
-        <meta name="description" content="Luxury bangles and jewellery collection" />
+        {/* Meta description */}
+        <meta
+          name="description"
+          content="Luxury bangles and jewellery collection"
+        />
       </Head>
 
+      {/* Route Loader */}
+      <RouteLoader />
+
+      {/* Page Content */}
       <Component {...pageProps} />
     </>
   );
