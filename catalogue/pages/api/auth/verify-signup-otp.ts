ecc84280fc45922d298b5e7db1294ebdae977222
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-16 17:17:32 +0300

    Update verify-signup-otp.ts

--- catalogue/pages/api/auth/verify-signup-otp.ts
+++ catalogue/pages/api/auth/verify-signup-otp.ts
@@ -25,7 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   });
 
   // Delete temp OTP
-  await prisma.signupOtp.delete({ where: { id: tempSignup.id } });
+  await prisma.PendingUser.delete({ where: { id: tempSignup.id } });
 
   res.status(200).json({ message: "Account created successfully!" });
 }
