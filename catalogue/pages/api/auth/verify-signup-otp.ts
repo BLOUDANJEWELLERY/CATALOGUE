831b6708d588634da59042ebdb26d3eb6beff873
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-16 17:35:10 +0300

    Update verify-signup-otp.ts

--- catalogue/pages/api/auth/verify-signup-otp.ts
+++ catalogue/pages/api/auth/verify-signup-otp.ts
@@ -30,7 +30,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       lastName: pendingUser.lastName,
       email: pendingUser.email,
       password: pendingUser.password, // already hashed
-      isVerified: true,
     },
   });
 
