84970b9db504bdf8668c8abded783b3148aa3713
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-16 20:21:41 +0300

    Update next.config.ts

--- catalogue/next.config.ts
+++ catalogue/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["cdn.sanity.io"], // Allow Sanity-hosted images
+  },
 };
 
 export default nextConfig;
