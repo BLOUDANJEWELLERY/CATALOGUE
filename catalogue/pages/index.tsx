f895b2abaa2142e517e0e7fcfa2e4348ee1a96b2
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-17 23:44:27 +0300

    Update index.tsx

--- catalogue/pages/index.tsx
+++ catalogue/pages/index.tsx
@@ -778,6 +778,7 @@ return (
       {newItemSizes.includes("Adult") && (
         <input
           type="number"
+          inputMode="numeric"
           placeholder="Weight Adult (g)"
           value={newItemWeightAdult}
           onChange={(e) => setNewItemWeightAdult(e.target.value)}
@@ -787,6 +788,7 @@ return (
       {newItemSizes.includes("Kids") && (
         <input
           type="number"
+          inputMode="numeric"
           placeholder="Weight Kids (g)"
           value={newItemWeightKids}
           onChange={(e) => setNewItemWeightKids(e.target.value)}
@@ -896,6 +898,7 @@ return (
       {editSizes.includes("Adult") && (
         <input
           type="number"
+          inputMode="numeric"
           placeholder="Weight Adult (g)"
           value={editWeightAdult}
           onChange={(e) => setEditWeightAdult(e.target.value)}
@@ -905,6 +908,7 @@ return (
       {editSizes.includes("Kids") && (
         <input
           type="number"
+          inputMode="numeric"
           placeholder="Weight Kids (g)"
           value={editWeightKids}
           onChange={(e) => setEditWeightKids(e.target.value)}
