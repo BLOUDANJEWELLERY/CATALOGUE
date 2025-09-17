28648860f3dea0b7fff74bf29e02bd7659a8db12
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-17 07:56:57 +0300

    Update cropImage.ts

--- catalogue/utils/cropImage.ts
+++ catalogue/utils/cropImage.ts
@@ -1,28 +1,16 @@
 // utils/cropImage.ts
-export const getCroppedImg = (
-  imageSrc: string,
-  pixelCrop: { x: number; y: number; width: number; height: number },
-  rotation = 0 // in degrees
-): Promise<Blob> => {
+export const getCroppedImg = (imageSrc: string, pixelCrop: { x: number; y: number; width: number; height: number }): Promise<Blob> => {
   return new Promise((resolve, reject) => {
     const image = new Image();
     image.src = imageSrc;
     image.crossOrigin = "anonymous";
-
     image.onload = () => {
       const canvas = document.createElement("canvas");
       canvas.width = pixelCrop.width;
       canvas.height = pixelCrop.height;
       const ctx = canvas.getContext("2d");
+
       if (!ctx) return reject("Cannot get canvas context");
-
-      // Convert degrees to radians
-      const rotRad = (rotation * Math.PI) / 180;
-
-      // Move to center of canvas
-      ctx.translate(canvas.width / 2, canvas.height / 2);
-      ctx.rotate(rotRad);
-      ctx.translate(-canvas.width / 2, -canvas.height / 2);
 
       ctx.drawImage(
         image,
@@ -41,7 +29,6 @@ export const getCroppedImg = (
         resolve(blob);
       }, "image/jpeg");
     };
-
     image.onerror = (err) => reject(err);
   });
 };
